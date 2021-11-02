ea472800b0fb45affad9734e4d7f05a7906c5546
juanvicente-dm <juanvicente.dm@gmail.com>
2021-11-02 02:06:48 -0300

    agregado navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 
 import './App.css';
+import NavBar from './components/navbar';
+
 
 function App() {
-
+  return (
+    <div className="App">
+      <NavBar/>
+     
+    </div>
+  )
 }
 
 export default App;
