3b7b0b26e700f7cc392c372ffa4d25c16bc49461
juanvicente-dm <juanvicente.dm@gmail.com>
2021-11-02 02:12:13 -0300

    agregado navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -15,7 +15,7 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
-import Button from '@mui/material/Button';
+
 
 
 const Search = styled('div')(({ theme }) => ({
